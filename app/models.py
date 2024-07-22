81b584778ebc1fe597430e9eaa2f5d16e5513f7b
Luiz Fernando Balbino <106929467+NandoLu@users.noreply.github.com>
2024-07-22 10:06:55 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -3,48 +3,51 @@
 from django.db import models
 
 class Partido(models.Model):
-    partido_político = models.CharField(max_length=100, verbose_name="Nome do partido")
+    partido_politico = models.CharField(max_length=100, verbose_name="Nome do partidor")
     def __str__(self):
-        return self.partido_político
+        return self.partido_politico
     class Meta:
         verbose_name = "Partido"
         verbose_name_plural = "Partidos"
-
+        
 class Jogador(models.Model):
     nome_jogador = models.CharField(max_length=100, verbose_name="Nome do jogador")
-    partido_politico = models.ForeignKey(Partido, verbose_name="Nome do partido do jogador")
+    partido_politico = models.ForeignKey(Partido, on_delete=models.CASCADE, verbose_name="Nome do partido")
     def __str__(self):
         return f"{self.nome_jogador}, {self.partido_politico}"
     class Meta:
-        verbose_name = "Jogador"
-        verbose_name_plural = "Jogadores"
+        verbose_name = "Nome do jogador"
+        verbose_name_plural = "Nomes dos jogadores"
+
         
 class Cenario(models.Model):
     cenario_nivel = models.IntegerField(verbose_name="Nível do cenário")
     descricao_cenario = models.CharField(max_length=100, verbose_name="Descrição do cenário")
     impacto_cenario = models.CharField(max_length=100, verbose_name="Impacto do cenário")
-    def __str__(self):
-        return f"{self.cenario_nivel}, {self.descricao_cenario}, {self.impacto_cenario}"
+    def __str__(self) -> str:
+        return f'{self.cenario_nivel}, {self.descricao_cenario}, {self.impacto_cenario}'
     class Meta:
-        verbose_name = "Cenario"
+        verbose_name = "Cenário"
         verbose_name_plural = "Cenários"
-
-class Desempenho(models.Model):
-    nome_jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE, verbose_name="Nome do jogador do desempenho")
-    popularidade_final = models.IntegerField(verbose_name="Popularidade final")
-    economia_final = models.IntegerField(verbose_name="Economia final")
-    def __str__(self):
-        return f"{self.nome_jogador}, {self.popularidade_final}, {self.economia_final}"
-    class Meta:
-        verbose_name = "Desempenho"
-        verbose_name_plural = "Desempenhos"
+         
 
 class Configuracao(models.Model):
     nome_jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE, verbose_name="Nome do jogador")
     idioma = models.CharField(max_length=9, verbose_name="Idioma escolhido")
     tema = models.CharField(max_length=6, verbose_name="Tema escolhido")
     def __str__(self):
-        return f'{self.nome_jogador}, {self.idioma}, {self.tema}'
+        return f'{self.idioma}, {self.tema}'
     class Meta:
-        verbose_name = "Configuracao"
-        verbose_name_plural = "Configuracoes"
+        verbose_name = "Configuração"
+        verbose_name_plural = "Configurações"
+    
+    
+class Desempenho(models.Model):
+    nome_jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE, verbose_name="Nome do jogador")
+    popularidade_final = models.IntegerField(verbose_name="Popularidade final")
+    economia_final = models.IntegerField(verbose_name="Economia final")
+    def __str__(self):
+        return f'{self.nome_jogador}, {self.popularidade_final}, {self.economia_final}'
+    class Meta:
+        verbose_name = "Desempenho"
+        verbose_name_plural = "Desempenhos"
--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,17 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic import TemplateView
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("app.urls")),
+    path('login/', LoginView.as_view(), name="login"),
+    # path('livros/', LivrosView.as_view(), name='livros'),
+    # path('reserva/', EmprestimoView.as_view(),name='reserva'),
+    # path('cidade/', CidadeView.as_view(),name='cidade'),
+    # path('autor/', AutoresView.as_view(), name='autor'),
+    # path('editor/', EditorasView.as_view(),name='editora'),
+    # path('leitor/', UsuarioView.as_view(),name='leitor'),
+    # path('genero/', GenerosView.as_view(),name='genero'),
 ]
